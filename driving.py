a312d85cfcd2c352cab9c391ecf0455c7ca02acd
Danny LEE <danny7616kimo@hotmail.com>
2020-04-22 22:35:17 +0800

    add US

--- driving.py
+++ driving.py
@@ -6,5 +6,10 @@ if country == '台灣':
 		print('你可以考駕照啦!')
 	else:
 		print('不能考駕照啦~')
+elif country == '美國':
+	if age >= 16:
+		print('那妳可以開車 美國爸爸')
+	else:
+		print('再等一下喔喔喔')
 else:
-	print('我不知道')
+	print('不關我事情')
